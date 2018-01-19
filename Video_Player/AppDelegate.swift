d902f8812dab4064d66ffd7949d0bedc04d1f493
HsiaoAi <ilct227@gmail.com>
2018-01-19 14:16:18 +0800

    Configure UI

--- Video_Player/AppDelegate.swift
+++ Video_Player/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,6 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         let viewController = ViewController()
         let navigationViewController = UINavigationController(rootViewController: viewController)
+        UINavigationBar.appearance().barStyle = .blackOpaque
         window?.rootViewController = navigationViewController
         
         // Setup IQKeyboard
--- Video_Player/ViewController.swift
+++ Video_Player/ViewController.swift
@@ -9,16 +9,82 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Properties
+    let searchBar = UISearchBar()
+    lazy var buttonsPanelView: UIView = {
+        let view = UIView(frame: CGRect(x: 0, y: 623, width: 375, height: 44))
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    lazy var playbutton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 20, y: 636, width: 33, height: 19))
+        button.tintColor = .white
+        button.setTitle(NSLocalizedString("Play", comment: ""), for: .normal)
+        button.tintColor = .white
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        return button
+    }()
+    
+    lazy var mutebutton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 320, y: 636, width: 39, height: 19))
+        button.tintColor = .white
+        button.setTitle(NSLocalizedString("Mute", comment: ""), for: .normal)
+        button.tintColor = .white
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        return button
+    }()
+    
+    
+    // View Life cycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .black
-        // Do any additional setup after loading the view, typically from a nib.
+        setupUI()
+        setupSearchBar()
+        setupNavigationBar()
+    }
+    
+    func setupUI() {
+        view.backgroundColor = UIColor(red: 8 / 255.0, green: 21 / 255.0, blue: 35 / 255.0, alpha: 1.0)
+        self.view.addSubview(buttonsPanelView)
+        self.view.addSubview(playbutton)
+        self.view.addSubview(mutebutton)
+    }
+    
+    func setupNavigationBar() {
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = UIColor.clear
+    }
+    
+    func setupSearchBar() {
+        // ToDo palceholder text style
+        searchBar.sizeToFit()
+        searchBar.searchBarStyle = .prominent
+        searchBar.placeholder = NSLocalizedString("Enter URL of video", comment: "")
+        searchBar.sizeToFit()
+        searchBar.isTranslucent = false
+        searchBar.backgroundImage = UIImage()
+        searchBar.delegate = self
+        searchBar.resignFirstResponder()
+        navigationItem.titleView = searchBar
+    }
+    
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+}
 
+extension ViewController: UISearchBarDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.searchBar.endEditing(true)
+    }
+    
+    func searchVideo() {
+        
+    }
 }
