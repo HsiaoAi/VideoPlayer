fefff22f134a745371c499c3753b9adc9e9cf217
HsiaoAi <ilct227@gmail.com>
2018-01-19 13:31:22 +0800

    Init

---
+++ Video_Player/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Video_Player
+//
+//  Created by Hsiao Ai LEE on 19/01/2018.
+//  Copyright © 2018 Hsiao Ai LEE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black 
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
